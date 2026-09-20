15e605b469e2c7115bd77a0807babd69cb25c034
endlesscod1ng <endlesscoding@yahoo.com>
2026-09-20 22:58:20 +0300

    cr a/thms

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,21 +1,30 @@
 import { Routes, Route } from "react-router";
-import s from "./App.module.scss";
 import { HomePage } from "../../pages/HomePage";
 import { ArticlesPage } from "../../pages/ArticlesPage";
+import { Suspense, useState } from "react";
+import "../styles/index.scss";
 
 export function App() {
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
   return (
-    <div className={s.app}>
-      <Routes>
-        <Route
-          path={"/"}
-          element={<HomePage />}
-        />
-        <Route
-          path={"/articles"}
-          element={<ArticlesPage />}
-        />
-      </Routes>
+    <div className={`app ${theme}`}>
+      <Suspense fallback={<div>{"Loading..."}</div>}>
+        <header>
+          <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            Theme
+          </button>
+        </header>
+        <Routes>
+          <Route
+            path={"/"}
+            element={<HomePage />}
+          />
+          <Route
+            path={"/articles"}
+            element={<ArticlesPage />}
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
